89dde73516cfa0f95e5c52bd2b9a6641828bfed9
PEFR <francesco.pezzati@maior.it>
2020-12-16 17:07:02 +0100

    Events sent and received but no payload...

--- team-one/src/index.js
+++ team-one/src/index.js
@@ -3,6 +3,9 @@ window.customElements.define('team-one', class TeamOne extends HTMLElement {
   constructor() {
     super();
     this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+    window.addEventListener('greet', (msg) => {
+      console.log("a message arrives: " + JSON.stringify(msg));
+    });
   }
   connectedCallback() {
     console.log('connected!');
--- team-zero/src/index.js
+++ team-zero/src/index.js
@@ -8,14 +8,20 @@ tmplt.innerHTML = `
 </div>
 `;
 
+var input = {};
+
 window.customElements.define('team-zero', class TeamZero extends HTMLElement {
   constructor() {
     super();
     this._shadowRoot = this.attachShadow({ 'mode': 'open' });
     this._shadowRoot.appendChild(tmplt.content.cloneNode(true));
     this.button = this._shadowRoot.getElementById('sendbtn');
+    input = this._shadowRoot.getElementById('sendnpt');
     this.button.addEventListener('click', function(evt) {
-      console.log('clicked');
+      var msg = { bubbles: true, detail: {} };
+      msg.detail.name = input.value;
+      console.log('sending msg: ' + JSON.stringify(msg))
+      window.dispatchEvent(new CustomEvent('greet', msg));
     });
   }
   connectedCallback() {
